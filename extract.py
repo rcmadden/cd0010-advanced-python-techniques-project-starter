b2b28dd15314b371e2d3e2d1ec20c12a8ac43725
rcmadden <dataengineer007@gmail.com>
2022-03-25 20:52:37 -0700

    rebuilt using dict instead of lists

--- extract.py
+++ extract.py
@@ -13,6 +13,7 @@ line, and uses the resulting collections to build an `NEODatabase`.
 You'll edit this file in Task 2.
 """
 import csv
+from dataclasses import fields
 import json
 
 from models import NearEarthObject, CloseApproach
@@ -29,7 +30,23 @@ def load_neos(neo_csv_path):
     with open(neo_csv_path) as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
-            neo_collection += [[row['id'], row['pdes'], row['name'] , row['diameter'], row['pha'], row['neo']]]
+            # list of neos:
+            # neo_collection += [[row['id'], row['pdes'], row['name'] , row['diameter'], row['pha'], row['neo']]]
+            # neo_collection.append(row)
+
+            # neo_collection += [dict(zip(['id', 'pdes', 'name', 'diameter', 'pha', 'neo', 'full_name'], [row['id'], row['pdes'], row['name'] , row['diameter'], row['pha'], row['neo'], row['full_name']]))]
+
+            # print(dict([('id', row['id']),('pdes',row['pdes']), ('name', row['name']),('diameter', row['diameter']),('pha', row['pha']),('neo', row['neo']),('full_name', row['full_name'])]))
+
+            #  neo_collection.append((dict([('id', row['id']),('pdes',row['pdes']), ('name', row['name']),('diameter', row['diameter']),('pha', row['pha']),('neo', row['neo']),('full_name', row['full_name'])])))
+
+             neo_collection.append((dict([('id', row['id']),('designation',row['pdes']), ('name', row['name']),('diameter', row['diameter']),('hazardous', row['pha']),('neo', row['neo']),('full_name', row['full_name'])])))
+
+            # neo_collection += dict([('id', row['id']),('pdes',row['pdes']), ('name', row['name']),('diameter', row['diameter']),('pha', row['pha']),('neo', row['neo']),('full_name', row['full_name'])])
+
+            # neo_collection = dict.fromkeys([('id', row['id']),('pdes',row['pdes']), ('name', row['name']),('diameter', row['diameter']),('pha', row['pha']),('neo', row['neo']),('full_name', row['full_name'])])
+
+            # neo_collection['id'] = row['id'],neo_collection['pdes'] = row['pdes'], neo_collection['name'] = row['name'],neo_collection['diameter'] = row['diameter'],neo_collection['pha'] = row['pha'],neo_collection['neo'] = row['neo']
 
     return neo_collection
 
@@ -42,15 +59,25 @@ def load_approaches(cad_json_path):
     """
     # TODO: Load close approach data from the given JSON file.
     cad_collection = []
-    # with openq('data/cad_10.json', 'r') as json_file:
+
+    # with open('data/cad_10.json', 'r') as json_file:
     with open(cad_json_path, 'r') as json_file:
         json_reader = json.load(json_file)
+
+    # list comprehension: # cad_comprehension = [cad for cad in json_reader['data']]
+
     for i in range(len(json_reader['data'])):
         # cad_collection += [dict(zip(json_reader['fields'],json_reader['data'][i]))]  
-        # cad_collection += json_reader['fields'][0], json_reader['fields'][3], json_reader['fields'][5], json_reader['fields'][8], json_reader['fields'][1]  
-        cad_collection += [[json_reader['data'][i][0], json_reader['data'][i][3], json_reader['data'][i][5], json_reader['data'][i][8], json_reader['data'][i][1]]]
-   
+        # cad_collection += [dict(zip([json_reader['fields'][0], json_reader['fields'][3], json_reader['fields'][4], json_reader['fields'][7], json_reader['fields'][1]], [json_reader['data'][i][0], json_reader['data'][i][3], json_reader['data'][i][4], json_reader['data'][i][7], json_reader['data'][i][1]]))] 
+
+    # def __init__(self, _designation='', time=None, distance=0.0, velocity=0.0, orbit_id=None, neo=None):
+
+        cad_collection += [dict(zip(['_designation', 'time', 'distance', 'velocity', 'orbit_id'], [json_reader['data'][i][0], json_reader['data'][i][3], json_reader['data'][i][4], json_reader['data'][i][7], json_reader['data'][i][1]]))] 
+        # fields        # cad_collection += json_reader['fields'][0], json_reader['fields'][3], json_reader['fields'][4], json_reader['fields'][7], json_reader['fields'][1]  
+        # values list        # cad_collection += [[json_reader['data'][i][0], json_reader['data'][i][3], json_reader['data'][i][4], json_reader['data'][i][7], json_reader['data'][i][1]]]
+
     print(cad_collection)
     return cad_collection
 
 # load_approaches('data/cad_10.json')
+# Required - des, cd, dist, and v_rel + orbit_id
--- models.py
+++ models.py
@@ -18,6 +18,8 @@ quirks of the data set, such as missing names and unknown diameters.
 You'll edit this file in Task 1.
 """
 from ast import Pass
+# from asyncio.windows_events import NULL
+from curses.ascii import NUL
 from time import time
 from unicodedata import name
 from helpers import cd_to_datetime, datetime_to_str
@@ -37,8 +39,8 @@ class NearEarthObject:
     """
     # TODO: How can you, and should you, change the arguments to this constructor?
     # If you make changes, be sure to update the comments in this file.
-    # def __init__(self, **info):
-    def __init__(self, id, designation='', name=None, diameter=float('nan'), hazardous=False):
+    def __init__(self, **kwargs):
+    # def __init__(self, id, designation='', name=None, diameter=float('nan'), hazardous=False, full_name=None, approaches=[]):
 
         """Create a new `NearEarthObject`.
 
@@ -53,24 +55,36 @@ class NearEarthObject:
         # self.name = None
         # self.diameter = float('nan')
         # self.hazardous = False
-        self.id = id
-        self.designation = designation # unique
-        self.name = name # may be missing but will have id
-        self.diameter = diameter # optional
-        self.hazardous = hazardous
+        # self.id = id
+        # self.designation = designation # unique
+        # self.name = name # may be missing but will have id
+        # self.diameter = diameter # optional
+        # self.hazardous = hazardous
+        # self.full_name = full_name # explore the datas full_name field
 
-        # Create an empty initial collection of linked approaches.
-        self.approaches = []
+        # # Create an empty initial collection of linked approaches.
+        # self.approaches = approaches
+        print(kwargs.items())
+        #  setdefault(key[, default])
+        
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+            if hasattr(self,'diameter') and type(self.diameter) == str:
+                if(self.diameter != ''):
+                    self.diameter = float(self.diameter)  
+                else:
+                    self.diameter = float('nan')
 
     @property
     def fullname(self):
         """Return a representation of the full name of this NEO."""
         # TODO: Use self.designation and self.name to build a fullname for this object.
-        if self.name == None:
+        if self.name == None or self.name == '':
             fullname = self.designation
         else:
             fullname = self.designation + ' ' + self.name
-        return f"NEO Fullname: {fullname}"
+        return f"NEO Fullname: {fullname} {self.full_name}"
 
     def __str__(self):
         """Return `str(self)`."""
@@ -83,12 +97,15 @@ class NearEarthObject:
         else: 
             hazard_staus = 'is not likely hazardous'
 
-        return f"{self.designation} ({self.fullname}) has a diameter of {self.diameter:.3f} km and {hazard_staus} id: {self.id}"
+        return f"{self.designation} fullname ({self.fullname}) full_name {self.full_name}  has a diameter of {self.diameter:.3f} km and {hazard_staus} id: {self.id}"
 
     def __repr__(self):
         """Return `repr(self)`, a computer-readable string representation of this object."""
+        # return f"NearEarthObject(id={self.id}, designation={self.designation!r}, name={self.name!r}, " \
+        #        f"diameter={self.diameter:.3f}, hazardous={self.hazardous!r})"
         return f"NearEarthObject(id={self.id}, designation={self.designation!r}, name={self.name!r}, " \
                f"diameter={self.diameter:.3f}, hazardous={self.hazardous!r})"
+
 
 
 class CloseApproach:
@@ -106,8 +123,8 @@ class CloseApproach:
     """
     # TODO: How can you, and should you, change the arguments to this constructor?
     # If you make changes, be sure to update the comments in this file.
-    # def __init__(self, **info):
-    def __init__(self, _designation='', time=None, distance=0.0, velocity=0.0, orbitID=None, neo=None):
+    def __init__(self, neo=None, **kwargs):
+    # def __init__(self, _designation='', time=None, distance=0.0, velocity=0.0, orbit_id=None, neo=None):
 
         """Create a new `CloseApproach`.
 
@@ -118,27 +135,31 @@ class CloseApproach:
         # You should coerce these values to their appropriate data type and handle any edge cases.
         # The `cd_to_datetime` function will be useful.
         # self._designation = ''
-        if time == None:
-            self.time = None
-        else:
-            self.time = cd_to_datetime(time)  # TODO: Use the cd_to_datetime function for this attribute.
         # self.distance = 0.0
         # self.velocity = 0.0
-        self._designation = _designation
-        
-        # if self.time == None:
-        #     Pass
-        # else:
-        #     self.time = cd_to_datetime(time)  # TODO: Use the cd_to_datetime function for this attribute.
-
-        self.distance = float(distance)
-        self.velocity = float(velocity)
+        # self._designation = _designation
+        # self.distance = float(distance)
+        # self.velocity = float(velocity)
 
         # Create an attribute for the referenced NEO, originally None.
-        self.neo = neo
+        self.neo = None
         
-        self.orbitID = orbitID
+        # self.orbit_id = orbit_id # explore the values in this field
+        # dict.setdefault(key, None)
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
+            if hasattr(self,'distance') and type(self.distance) == str:
+                if(self.distance != ''):
+                    self.distance = float(self.distance)
+                else:
+                    self.distance = float(0.0)
+     
+            if hasattr(self,'velocity') and type(self.velocity) == str:
+                if(self.velocity != ''):
+                    self.velocity = float(self.velocity)
+                else:
+                    self.velocity = float(0.0)
     @property
     def time_str(self):
         """Return a formatted representation of this `CloseApproach`'s approach time.
@@ -155,11 +176,14 @@ class CloseApproach:
         # xTODO: Use this object's `.time` attribute and the `datetime_to_str` function to
         # build a formatted representation of the approach time.
         # TODO: Use self.designation and self.name to build a fullname for this object.
-        if self.time == None:
-            approach_time = None
-        else:
-            self.time
+        # print('BEFORE', self.time, type(self.time))
 
+        # if self.time == '':
+        #     self.time = None
+        # else:
+        #     self.time = cd_to_datetime(self.time)  # TODO: Use the cd_to_datetime function for this attribute.
+        # print('AFTER', self.time, type(self.time))
+        # print('datetime_to_str', datetime_to_str(self.time), type(self.time))
         # return f"CloseAproach Time({self.time})"
         return self.time
 
@@ -168,9 +192,15 @@ class CloseApproach:
         # TODO: Use this object's attributes to return a human-readable string representation.
         # The project instructions include one possibility. Peek at the __repr__
         # method for examples of advanced string formatting.
-        return f"On {self.time_str}, '{self._designation} ({self.neo})' approaches Earth at a distance of {self.distance:.2f} au and a velocity of {self.velocity:.2f} km/s. Orbit Id {self.orbitID}"
+        return f"On {self.time_str!r}, '{self._designation} (NEO {self.neo})' approaches Earth at a distance of {self.distance:.2f} au and a velocity of {self.velocity:.2f} km/s. Orbit Id {self.orbit_id}"
 
     def __repr__(self):
         """Return `repr(self)`, a computer-readable string representation of this object."""
         return f"CloseApproach(_designation={self._designation}, time={self.time_str!r}, distance={self.distance:.2f}, " \
                f"velocity={self.velocity:.2f}."
+
+# NearEarthObject(**{'id': 'a0000433', 'designation': '433', 'name': 'Eros', 'diameter': '16.84', 'hazardous': 'N', 'neo': 'Y', 'full_name': '433 Eros (A898 PA)'})
+
+# NearEarthObject(**{'id': 'a0000433', 'designation': '433', 'name': 'Eros', 'diameter': '', 'hazardous': 'N', 'neo': 'Y', 'full_name': '433 Eros (A898 PA)'})
+
+# CloseApproach(**{'_designation': '170903', 'time': '1900-Jan-01 00:11', 'distance': '0.0921795123769547', 'velocity': '16.7523040362574', 'orbit_id': '105'})
