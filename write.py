d86fda068d643f72de42d984fe75435e71b76ba3
rcmadden <dataengineer007@gmail.com>
2022-03-28 21:06:55 -0700

    write to csv and json naively working wip

--- write.py
+++ write.py
@@ -12,6 +12,8 @@ You'll edit this file in Part 4.
 """
 import csv
 import json
+from re import L
+from unittest import result
 
 
 def write_to_csv(results, filename):
@@ -24,12 +26,25 @@ def write_to_csv(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
+    # fieldnames = (
+    #     'datetime_utc', 'distance_au', 'velocity_km_s',
+    #     'designation', 'name', 'diameter_km', 'potentially_hazardous'
+    # )
     fieldnames = (
-        'datetime_utc', 'distance_au', 'velocity_km_s',
-        'designation', 'name', 'diameter_km', 'potentially_hazardous'
+        'time', 'distance', 'velocity',
+        '_designation', 'name', 'diameter', 'hazardous', 'neo'
     )
     # TODO: Write the results to a CSV file, following the specification in the instructions.
+    with open(filename, 'w') as csv_outfile:
+        writer = csv.DictWriter(csv_outfile, fieldnames=fieldnames)
+        print('writer: ', writer.fieldnames)
+        writer.writeheader()
+        # print('results: ', results)
+        # print('results: ', list(results))
 
+        for elem in results:
+            print(elem)
+            writer.writerow(elem)
 
 def write_to_json(results, filename):
     """Write an iterable of `CloseApproach` objects to a JSON file.
@@ -43,3 +58,18 @@ def write_to_json(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     # TODO: Write the results to a JSON file, following the specification in the instructions.
+    with open(filename, 'w') as json_outfile:
+        # json.dump(results, json_outfile)
+        for elem in results:
+            # print(elem)
+            json.dump(elem, json_outfile, indent=4)
+           
+            # elem_dict = json.dumps(elem)
+            # print(elem_dict)
+            # print(json.dumps(elem_dict, indent=4, separators=(',\n', ': ')))
+           
+            #   json.dumps(elem, indent=4, separators=(',', ': '))
+
+
+
+
